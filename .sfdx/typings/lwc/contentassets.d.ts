f358754be46a7204a7a133497b9776d97ff94551
Sumanga <restituto.m.sumanga@accenture.com>
2019-11-05 11:44:19 +0800

    update commit

---
+++ .sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/contentAssetUrl/EasySpacesTile01" {
+    var EasySpacesTile01: string;
+    export default EasySpacesTile01;
+}
+declare module "@salesforce/contentAssetUrl/EasySpacesTile011" {
+    var EasySpacesTile011: string;
+    export default EasySpacesTile011;
+}
